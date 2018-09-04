533ec2351bb103da3d715ba9d77c55c77f236335
spikespiegel5112 <583288277@qq.com>
2018-09-04 17:51:16 +0800

    prod

--- src/views/advertising/advertistingList.vue
+++ src/views/advertising/advertistingList.vue
@@ -377,6 +377,8 @@
               queryObj[item] = this.queryModel[item]
             }
           })
+        }else{
+          queryObj=this.queryModel;
         }
         this.$http.get(this.$baseUrl + this.advertisingListRequest, {
           params: Object.assign(queryObj, this.pagination)
